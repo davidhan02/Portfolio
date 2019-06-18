0e34f98a5f8bc76195f32fadaa5bf23699e7a640
David Han <davidhan.5.25@gmail.com>
2019-06-18 16:47:55 -0700

    Added to project server side validation

--- server/routes/middleware/validate/project.js
+++ server/routes/middleware/validate/project.js
@@ -5,32 +5,49 @@ module.exports = function projectValidator(data) {
   let errors = {};
 
   data.url = !isEmpty(data.url) ? data.url : '';
-  data.github = !isEmpty(data.github) ? data.github : '';
+  data.code = !isEmpty(data.code) ? data.code : '';
   data.text = !isEmpty(data.text) ? data.text : '';
   data.title = !isEmpty(data.title) ? data.title : '';
+  data.categories = !isEmpty(data.categories) ? data.categories : '';
 
-  if (!Validator.isEmpty(data.url) && !Validator.isURL(data.url)) {
-    errors.message = 'Given link must be a valid URL';
+  if (Validator.isEmpty(data.url)) {
+    errors.message = 'Given link field is required';
   }
 
-  if (!Validator.isEmpty(data.github) && !Validator.isURL(data.github)) {
-    errors.message = 'Given github must be a valid URL';
+  if (!Validator.equals('Offline') && !Validator.isURL(data.url)) {
+    errors.message = 'Given link must be a valid URL or Offline';
+  }
+
+  if (Validator.isEmpty(data.code)) {
+    errors.message = 'Code field is required';
+  }
+
+  if (!Validator.isURL(data.code)) {
+    errors.message = 'Given code must be a valid URL';
   }
 
   if (Validator.isEmpty(data.title)) {
     errors.message = 'Title field is required';
   }
 
-  if (!Validator.isLength(data.title, { min: 3, max: 100 })) {
-    errors.message = 'Project title must be between 3 and 100 characters';
+  if (!Validator.isLength(data.title, { min: 3, max: 50 })) {
+    errors.message = 'Project title must be between 3 and 50 characters';
   }
 
   if (Validator.isEmpty(data.text)) {
     errors.message = 'Project text field is required';
   }
 
-  if (!Validator.isLength(data.text, { min: 3, max: 1500 })) {
-    errors.message = 'Project text must be between 3 and 1500 characters';
+  if (!Validator.isLength(data.text, { min: 10, max: undefined })) {
+    errors.message = 'Project text must be at least 10 characters';
+  }
+
+  if (Validator.isEmpty(data.categories)) {
+    errors.message = 'Project categories field is required';
+  }
+
+  if (!Validator.isLength(data.categories, { min: 8, max: undefined })) {
+    errors.message = 'Project categories must be at least 8 characters';
   }
 
   return {
