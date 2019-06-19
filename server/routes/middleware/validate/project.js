77444fe8d75d10080b5167412b2c7be48b4b05e4
David Han <davidhan.5.25@gmail.com>
2019-06-19 15:10:42 -0700

    Refactor project validator

--- server/routes/middleware/validate/project.js
+++ server/routes/middleware/validate/project.js
@@ -4,50 +4,34 @@ const isEmpty = require('./isEmpty');
 module.exports = function project(data) {
   let errors = {};
 
-  data.url = !isEmpty(data.url) ? data.url : '';
-  data.code = !isEmpty(data.code) ? data.code : '';
-  data.text = !isEmpty(data.text) ? data.text : '';
-  data.title = !isEmpty(data.title) ? data.title : '';
-  data.categories = !isEmpty(data.categories) ? data.categories : '';
+  const required = ['url', 'code', 'text', 'title', 'categories'];
 
-  if (Validator.isEmpty(data.url)) {
-    errors.message = 'Given link field is required';
-  }
+  required.forEach(field => {
+    data[field] = !isEmpty(data[field]) ? data[field] : '';
+
+    if (Validator.isEmpty(data[field])) {
+      errors.message = `${field} entry is required`;
+    }
+  });
 
   if (!Validator.equals(data.url, 'Offline') && !Validator.isURL(data.url)) {
     errors.message = 'Given link must be a valid URL or Offline';
-  }
-
-  if (Validator.isEmpty(data.code)) {
-    errors.message = 'Code field is required';
-  }
-
-  if (!Validator.isURL(data.code)) {
-    errors.message = 'Given code must be a valid URL';
-  }
-
-  if (Validator.isEmpty(data.title)) {
-    errors.message = 'Title field is required';
   }
 
   if (!Validator.isLength(data.title, { min: 3, max: 50 })) {
     errors.message = 'Project title must be between 3 and 50 characters';
   }
 
-  if (Validator.isEmpty(data.text)) {
-    errors.message = 'Project text field is required';
-  }
-
   if (!Validator.isLength(data.text, { min: 10, max: undefined })) {
     errors.message = 'Project text must be at least 10 characters';
   }
 
-  if (Validator.isEmpty(data.categories)) {
-    errors.message = 'Project categories field is required';
-  }
-
   if (!Validator.isLength(data.categories, { min: 8, max: undefined })) {
     errors.message = 'Project categories must be at least 8 characters';
+  }
+
+  if (!Validator.isURL(data.code)) {
+    errors.message = 'Given code must be a valid URL';
   }
 
   return {
