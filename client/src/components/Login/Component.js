4cb66623276eed2c8e670df8a6d2dfc906943e67
David Han <davidhan.5.25@gmail.com>
2019-06-21 19:02:06 -0700

    Separating container vs presentational component in Login

--- client/src/components/Login/Component.js
+++ client/src/components/Login/Component.js
@@ -1,50 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Field } from 'redux-form';
 import Form from '../shared/form/Form';
 import renderField from '../shared/form/renderField';
 import SubmitButton from '../shared/form/SubmitButton';
 import { usernameValidator, passwordValidator } from '../../util/validators';
 
-class Login extends Component {
-  componentDidMount() {
-    this.redirectIfLoggedIn();
-  }
-
-  componentDidUpdate() {
-    this.redirectIfLoggedIn();
-  }
-
-  redirectIfLoggedIn() {
-    const { token, history } = this.props;
-    if (token) history.push('/dashboard');
-  }
-
-  onSubmit = formValues => {
-    this.props.submitLogin(formValues);
-  };
-
-  render() {
-    const { handleSubmit, loading } = this.props;
-    return (
-      <Form loading={loading} onSubmit={handleSubmit(this.onSubmit)}>
-        <Field
-          type="username"
-          name="username"
-          label="username"
-          component={renderField}
-          validate={usernameValidator}
-        />
-        <Field
-          type="password"
-          name="password"
-          label="password"
-          component={renderField}
-          validate={passwordValidator}
-        />
-        <SubmitButton type="submit">Log in</SubmitButton>
-      </Form>
-    );
-  }
-}
+const Login = ({ loading, handleSubmit }) => (
+  <Form loading={loading} onSubmit={handleSubmit}>
+    <Field
+      type="username"
+      name="username"
+      label="username"
+      component={renderField}
+      validate={usernameValidator}
+    />
+    <Field
+      type="password"
+      name="password"
+      label="password"
+      component={renderField}
+      validate={passwordValidator}
+    />
+    <SubmitButton type="submit">Log in</SubmitButton>
+  </Form>
+);
 
 export default Login;
--- client/src/components/Login/Container.js
+++ client/src/components/Login/Container.js
@@ -1,12 +1,37 @@
+import React, { Component } from 'react';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { reduxForm } from 'redux-form';
 import { submitLogin, clearError } from '../../actions/auth';
 import Login from './Component';
 
-const mapStateToProps = ({ auth: { token, loading } }) => ({
-  token,
-  loading
+class LoginContainer extends Component {
+  componentDidMount() {
+    this.redirectIfLoggedIn();
+  }
+
+  componentDidUpdate() {
+    this.redirectIfLoggedIn();
+  }
+
+  redirectIfLoggedIn() {
+    const { token, history } = this.props;
+    if (token) history.push('/dashboard');
+  }
+
+  onSubmit = formValues => {
+    this.props.submitLogin(formValues);
+  };
+
+  render() {
+    const { handleSubmit, loading } = this.props;
+    return <Login loading={loading} handleSubmit={handleSubmit(this.onSubmit)} />;
+  }
+}
+
+const mapStateToProps = ({ auth }) => ({
+  token: auth.token,
+  loading: auth.loading
 });
 
 const mapDispatchToProps = { submitLogin, clearError };
@@ -19,6 +44,4 @@ const enhance = compose(
   )
 );
 
-const LoginContainer = enhance(Login);
-
-export default LoginContainer;
+export default enhance(LoginContainer);
