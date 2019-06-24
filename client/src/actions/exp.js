709a7a3c267780d631edc7ab9f8621bd42a438bb
David Han <davidhan.5.25@gmail.com>
2019-06-23 21:53:45 -0700

    Created education and experience actions

---
+++ client/src/actions/exp.js
@@ -0,0 +1,54 @@
+import axios from 'axios';
+import history from '../util/history';
+import { getFirstProfile } from './profile';
+import { SET_ERROR, SET_PROFILE, SET_PROFILE_LOADING } from './types';
+
+export const setProfileLoading = () => ({
+  type: SET_PROFILE_LOADING
+});
+
+export const setError = err => ({
+  type: SET_ERROR,
+  payload: err.response.data
+});
+
+export const submitExp = (formValues, profileId) => async dispatch => {
+  dispatch(setProfileLoading());
+  try {
+    const response = await axios.post(`/api/profile/${profileId}/exp`, formValues);
+    dispatch({
+      type: SET_PROFILE,
+      payload: response.data
+    });
+    history.push('/profile');
+  } catch (err) {
+    dispatch(setError(err));
+  }
+};
+
+export const updateExp = (formValues, profileId, expId) => async dispatch => {
+  dispatch(setProfileLoading());
+  try {
+    const response = await axios.patch(
+      `/api/profile/${profileId}/edu/${expId}`,
+      formValues
+    );
+    dispatch({
+      type: SET_PROFILE,
+      payload: response.data
+    });
+    history.push('/profile');
+  } catch (err) {
+    dispatch(setError(err));
+  }
+};
+
+export const deleteExp = (profileId, expId) => async dispatch => {
+  dispatch(setProfileLoading());
+  try {
+    await axios.delete(`/api/profile/${profileId}/edu/${expId}`);
+    dispatch(getFirstProfile);
+  } catch (err) {
+    dispatch(setError(err));
+  }
+};
