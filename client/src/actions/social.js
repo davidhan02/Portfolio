faa14e01e81fba1765a807579cc6ffcbcf918ae9
David Han <davidhan.5.25@gmail.com>
2019-06-23 22:16:17 -0700

    Created social actions

---
+++ client/src/actions/social.js
@@ -0,0 +1,57 @@
+import axios from 'axios';
+import history from '../util/history';
+import { getFirstProfile } from './profile';
+import { SET_ERROR, SET_PROFILE, SET_PROFILE_LOADING } from './types';
+
+export const setProfileLoading = () => ({
+  type: SET_PROFILE_LOADING
+});
+
+export const setError = err => ({
+  type: SET_ERROR,
+  payload: err.response.data
+});
+
+export const submitSocial = (formValues, profileId) => async dispatch => {
+  dispatch(setProfileLoading());
+  try {
+    const response = await axios.post(
+      `/api/profile/${profileId}/social`,
+      formValues
+    );
+    dispatch({
+      type: SET_PROFILE,
+      payload: response.data
+    });
+    history.push('/profile');
+  } catch (err) {
+    dispatch(setError(err));
+  }
+};
+
+export const updateSocial = (formValues, profileId) => async dispatch => {
+  dispatch(setProfileLoading());
+  try {
+    const response = await axios.patch(
+      `/api/profile/${profileId}/social`,
+      formValues
+    );
+    dispatch({
+      type: SET_PROFILE,
+      payload: response.data
+    });
+    history.push('/profile');
+  } catch (err) {
+    dispatch(setError(err));
+  }
+};
+
+export const deleteSocial = profileId => async dispatch => {
+  dispatch(setProfileLoading());
+  try {
+    await axios.delete(`/api/profile/${profileId}/social`);
+    dispatch(getFirstProfile);
+  } catch (err) {
+    dispatch(setError(err));
+  }
+};
