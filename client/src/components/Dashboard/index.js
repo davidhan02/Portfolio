4fa6f6a4fb2ac6d30474d59c7ada4409ba7dd56d
David Han <davidhan.5.25@gmail.com>
2019-06-25 17:29:12 -0700

    Added simple delete link to Dashboard

--- client/src/components/Dashboard/index.js
+++ client/src/components/Dashboard/index.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 
 const Dashboard = () => (
   <>
+    <Link to="/profile">Delete Profile</Link>
+    <br />
     <Link to="/profile/form">Edit Profile</Link>
     <br />
     <Link to="/projects/form">Add New Project</Link>
