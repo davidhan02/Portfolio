59f284831e28ab28dba0ae16f6ff2c23c40282f3
David Han <davidhan.5.25@gmail.com>
2019-06-27 22:50:14 -0700

    Fixed key mapping problem in MessageList

--- client/src/components/MessageList/Component.js
+++ client/src/components/MessageList/Component.js
@@ -2,6 +2,6 @@ import React from 'react';
 import MessageHeaderContainer from '../Message/Header/Container';
 
 const MessageList = ({ messages }) =>
-  messages.map(msg => <MessageHeaderContainer message={msg} details />);
+  messages.map(msg => <MessageHeaderContainer key={msg.id} message={msg} details />);
 
 export default MessageList;
